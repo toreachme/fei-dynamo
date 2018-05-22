b827096a28a7135da95f9fece95a8ff85210eafa
Emmanuel <emonuoha@gmail.com>
2018-05-22 19:48:19 -0400

    created events page

---
+++ events.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<?php require_once('./static/header.php');?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md text-black bg-white p-5">
+			<h1 class="font-weight-bold fs-49">Events and Talks</h1>
+			<p class="text-secondary w-75 fs-20">
+			Our team is constantly traveling around the globe to share our story, as well as success stories of our clients. Check out our events – the ones that we are hosting, sponsoring and attending.</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md p-0">
+			<div class="border myborder-color-blue p-4 boxed-darkblue-bg text-center">Upcoming Events</div>
+		</div>
+		<div class="col-md p-0">
+			<div class="border myborder-color-blue border-left-0 p-4 text-center">Past Events</div>
+		</div>
+	</div>
+
+	<!-- Tab panes -->
+	<div class="tab-content">
+		<div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+		<div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+		<div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">...</div>
+		<div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab">...</div>
+	</div>
+</div>
+
+
+
+<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.4/holder.js"></script>
+
+</body>
+</html>
