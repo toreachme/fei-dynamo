d350315b07f99e981e7ccf93e1356b78be256b1d
Emmanuel <emonuoha@gmail.com>
2018-06-11 09:01:50 -0400

    added the buy (listing buy page) page

---
+++ listing/buy.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+<?php require_once('../static/header.php');?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md bg-top-grey fc-dark-grey py-4 px-5 mx-5 my-4">
+			<p class="fs-18 m-0">Your PoF is only verified for $222000, you need to have verified atleast $258859295-10% to receive the teaser.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md text-black bg-white px-5 py-3">
+			<p class="w-75 fc-grey fs-18">Listed , Direct (Traditional)</p>
+			<h1 class="font-weight-bold fs-49">App and Software Blog</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="pl-5 pb-5">
+			<a href="#"><p class="font-weight-bold fc-link fs-14"><i class="fas fa-long-arrow-alt-right"></i><span class="pr-3">&nbsp;</span>Send to a friend</p></a>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="my-3 px-3 font-weight-bold fs-14">
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item"><div class="fs-18">Details</div></li>
+					<li class="list-group-item"><span class="">Sale Price</span><span class="float-right">$1000</span></li>
+					<li class="list-group-item"><span>Yearly Net Profit</span><span class="float-right">$3400</span></li>
+					<li class="list-group-item"><span>Yearly Revenue</span><span class="float-right">$7500</span></li>
+				</ul>
+			</div>
+
+			<div>
+				<ul class="list-group list-group-flush font-weight-bold fs-14">
+					<li class="list-group-item border-0"><div class="fs-18">Main Benefits</div></li>
+					<li class="list-group-item border-0">565% average net margin</li>
+					<li class="list-group-item border-0">Scalable traffic sources</li>
+					<li class="list-group-item border-0">Perfect for a first-time buyer</li>
+					<li class="list-group-item border-0">Huge social media outreach and following</li>
+				</ul>
+			</div>
+
+			<div>
+				<div class="col-md-10 pt-2 mt-2">
+					<button type="submit" class="btn btn-success full-width">Download Teaser</button>
+				</div>
+				<div class="col-md-10 pt-2 mt-2">
+					<button type="submit" class="btn btn-success full-width">Request Prospectus</button>
+				</div>				
+				<div class="col-md-10 pt-2 mt-2">
+					<button type="submit" class="btn btn-success full-width">Download Letter of Intent for Approval</button>
+				</div>
+				<div class="col-md-10 pt-2 mt-2">
+					<button type="submit" class="btn btn-success full-width">Approve LOI</button>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-8">
+			<h3 class="font-weight-bold pt-4 pl-2 fs-24">Description</h3>
+			<div class="px-2 py-3 section-font fc-grey fs-18">
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+			</div>
+
+			<div>
+				<h4 class="font-weight-bold fs-18 my-4">Notes</h4>
+				<form>
+					<div class="form-row mb-3">
+						<div class="col-md">
+							<input class="form-control" type="text" placeholder="Type your note here...">
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="col-md-3 mt-2">
+							<p>Add note</p>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<!--footer container fluid-->
+<?php require_once('../static/footer.php');?>
+
+
+<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.4/holder.js"></script>
+	<script src="http://localhost/FE/js/onclick.js"></script>
+
+</body>
+</html>
